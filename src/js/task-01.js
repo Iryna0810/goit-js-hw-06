c35735e524641bd5e33851f27aaea286054ec579
Rew <andriyvak1233@gmail.com>
2023-02-23 22:27:58 +0200

    task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,3 +1,32 @@
-const itemAll = document.querySelectorAll('li.item');
-console.log(itemAll);
+const itemAll = document.querySelectorAll('.item');
+
+const getTotalitem = (array) => {
+    let total = 0;
+
+    array.forEach(item => {
+        total += 1;
+    })  return total;
+};
+
+console.log(`Number of categories: ${getTotalitem(itemAll)}`)
+
+const categories = document.querySelector('#categories');
+
+
+const firstCategory = categories.firstElementChild;
+console.log(`Category: ${firstCategory.firstElementChild.textContent}`);
+const firstCategoryEl = firstCategory.querySelectorAll("li");
+console.log(`Elements: ${getTotalitem(firstCategoryEl)}`)
+
+const secondCategory = firstCategory.nextElementSibling;
+console.log(`Category: ${secondCategory.firstElementChild.textContent}`);
+const secondCategoryEl = secondCategory.querySelectorAll("li");
+console.log(`Elements: ${getTotalitem(secondCategoryEl)}`)
+
+const thirdCategory = secondCategory.nextElementSibling;
+console.log(`Category: ${thirdCategory.firstElementChild.textContent}`);
+const thirdCategoryEl = thirdCategory.querySelectorAll("li");
+console.log(`Elements: ${getTotalitem(thirdCategoryEl)}`)
+
+    
 
