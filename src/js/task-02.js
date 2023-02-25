f9e0cac28ca4f48283614bc5be8725e812741aa8
Rew <andriyvak1233@gmail.com>
2023-02-25 06:20:12 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -10,15 +10,19 @@ const ingredients = [
 const ingredientsAll = document.querySelector('#ingredients')
 console.log(ingredientsAll);
 
+const liAll = [];
+
 ingredients.forEach(
   (ingredient) => {
     const itemLi = document.createElement('li');
     itemLi.className = "item";
     itemLi.textContent = ingredient;
-    
-    ingredientsAll.append(itemLi);
-    console.log(itemLi);
+    liAll.push(itemLi);
   }
 );
+
+liAll.map((itemLi) => ingredientsAll.append(itemLi));
+
+
 
 
