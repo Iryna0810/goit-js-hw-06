7da939632bf025aafc1f866d3915df63381e1b1a
Rew <andriyvak1233@gmail.com>
2023-02-27 23:22:35 +0200

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -5,11 +5,11 @@ console.log(lengthControl);
 input.addEventListener('blur', handlerInputSymbol);
 
 function handlerInputSymbol(event) {
-    // input.classList.add('valid')
     console.log(event.currentTarget.textContent);
     input.textContent = event.currentTarget.value;
     if (input.textContent.length < lengthControl) {
         input.classList.add('invalid');
+        return;
         } else input.classList.add('valid');
 }
 
