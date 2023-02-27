02574fc5626821ca7488e2f903cb5c811c7f5d3d
Rew <andriyvak1233@gmail.com>
2023-02-28 00:15:35 +0200

    Update task-07.js

---
+++ src/js/task-07.js
@@ -0,0 +1,7 @@
+const rangeInput = document.querySelector('#font-size-control');
+const output = document.querySelector('#text');
+
+
+rangeInput.addEventListener('input', (event) => {
+    output.style.fontSize = `${event.target.value}px`
+});
